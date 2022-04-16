50d773ffedf76c20119796e15653118d09016f29
Antonette Caldwell <pullmana8@gmail.com>
2022-04-15 22:29:35 -0500

    Needs to add (backend, ctcAlice.getInfo()); back on line 10, because I forgot it in tutorial 1. However, reach did not detect there was a missing semicolon.

--- 1-tutorial/index.mjs
+++ 1-tutorial/index.mjs
@@ -7,7 +7,7 @@ const accountAlice = await stdlib.newTestAccount(startingBalance);
 const accountBob = await stdlib.newTestAccount(startingBalance);
 
 const ctcAlice = accountAlice.contract(backend);
-const ctcBob = accountBob.contract(backend)
+const ctcBob = accountBob.contract(backend, ctcAlice.getInfo());
 await Promise.all([
     ctcAlice.p.Alice({
         // implement Alice's interact object here
